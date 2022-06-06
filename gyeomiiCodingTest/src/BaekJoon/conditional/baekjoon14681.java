8ae7cda7cd4df6a84db0b70d19c1697cd529ffc3
gyeomii <kimsg7388@gmail.com>
2022-06-06 17:24:18 +0900

    조건문

--- gyeomiiCodingTest/src/BaekJoon/conditional/baekjoon14681.java
+++ gyeomiiCodingTest/src/BaekJoon/conditional/baekjoon14681.java
@@ -5,10 +5,8 @@ import java.util.Scanner;
 public class baekjoon14681 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int x = sc.nextInt();
-		int y = sc.nextInt();
-		
-		
-
+		int x = sc.nextInt(); int y = sc.nextInt();
+		sc.close();
+		System.out.println(x>0?(y>0?1:4):(y>0?2:3));
 	}
 }
