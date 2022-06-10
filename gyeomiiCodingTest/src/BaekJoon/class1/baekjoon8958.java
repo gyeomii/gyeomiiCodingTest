471662fa7eec0f4ccbb217dd932bcd076b86f482
gyeomii <kimsg7388@gmail.com>
2022-06-10 16:55:47 +0900

    buffer reader

---
+++ gyeomiiCodingTest/src/BaekJoon/class1/baekjoon8958.java
@@ -0,0 +1,34 @@
+package BaekJoon.class1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class baekjoon8958 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int test = Integer.parseInt(br.readLine());
+		String arr[] = new String[test];
+
+		for (int i = 0; i < test; i++) {
+			arr[i] = br.readLine();
+		}
+
+		for (int i = 0; i < test; i++) {
+			int cnt = 0;
+			int sum = 0;
+			
+			for (int j = 0; j < arr[i].length(); j++) {
+				if (arr[i].charAt(j) == 'O') {
+					cnt++;
+				} else {
+					cnt = 0;
+				}
+				sum += cnt;
+			}
+			sb.append(sum).append('\n');
+		}
+		System.out.println(sb);
+	}
+}
