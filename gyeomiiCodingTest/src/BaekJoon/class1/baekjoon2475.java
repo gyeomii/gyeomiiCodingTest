9eb5e269925ff1319640771309971741534cbd29
gyeomii <kimsg7388@gmail.com>
2022-06-13 12:09:32 +0900

    class1++

---
+++ gyeomiiCodingTest/src/BaekJoon/class1/baekjoon2475.java
@@ -0,0 +1,19 @@
+package BaekJoon.class1;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class baekjoon2475 {
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int sum = 0;
+		for (byte b : br.readLine().getBytes()) {
+			if (b == 32) {
+			} else {
+				sum += (b - 48) * (b - 48);
+			}
+		}
+		System.out.println(sum % 10);
+	}
+}
---
+++ gyeomiiCodingTest/src/BaekJoon/class1/baekjoon2577.java
@@ -0,0 +1,12 @@
+package BaekJoon.class1;
+
+import java.util.Scanner;
+
+public class baekjoon2577 {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		String a = Integer.toString(sc.nextInt()*sc.nextInt()*sc.nextInt());
+		sc.close();
+		System.out.println(a);
+	}
+}
