12211365ddb4a163db99d6e55826eae83f94f820
gyeomii <kimsg7388@gmail.com>
2022-06-13 12:16:16 +0900

    class1++

--- gyeomiiCodingTest/src/BaekJoon/class1/baekjoon2577.java
+++ gyeomiiCodingTest/src/BaekJoon/class1/baekjoon2577.java
@@ -5,8 +5,16 @@ import java.util.Scanner;
 public class baekjoon2577 {
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
-		String a = Integer.toString(sc.nextInt()*sc.nextInt()*sc.nextInt());
+		String str = Integer.toString(sc.nextInt()*sc.nextInt()*sc.nextInt());
 		sc.close();
-		System.out.println(a);
+		for (int i = 0; i < 10; i++) {
+			int count = 0;
+			for (int j = 0; j < str.length(); j++) {
+				if((str.charAt(j) - '0') == i) {
+					count++;
+				}
+			}
+			System.out.println(count);
+		}
 	}
 }
