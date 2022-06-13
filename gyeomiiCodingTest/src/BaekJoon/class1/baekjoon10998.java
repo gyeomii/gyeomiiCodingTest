67abfaf786b6213b695257c788bb8f92b82d2af0
gyeomii <kimsg7388@gmail.com>
2022-06-13 16:47:40 +0900

    class1++ Fin

--- gyeomiiCodingTest/src/BaekJoon/class1/baekjoon10998.java
+++ gyeomiiCodingTest/src/BaekJoon/class1/baekjoon10998.java
@@ -1,5 +1,11 @@
 package BaekJoon.class1;
 
-public class baekjoon10998 {
+import java.util.Scanner;
 
+public class baekjoon10998 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt()*sc.nextInt();
+		System.out.println(n);
+		}
 }
