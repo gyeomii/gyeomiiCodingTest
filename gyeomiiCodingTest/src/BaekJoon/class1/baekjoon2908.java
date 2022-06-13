8ba676f4f4944d0fef221b4549c96f84d64b7016
gyeomii <kimsg7388@gmail.com>
2022-06-13 15:21:06 +0900

    class1++

--- gyeomiiCodingTest/src/BaekJoon/class1/baekjoon2908.java
+++ gyeomiiCodingTest/src/BaekJoon/class1/baekjoon2908.java
@@ -1,7 +1,15 @@
 package BaekJoon.class1;
 
+import java.util.Scanner;
+
 public class baekjoon2908 {
 	public static void main(String[] args) {
-
+		Scanner sc = new Scanner(System.in);
+		StringBuffer sb1 = new StringBuffer(sc.next());
+		StringBuffer sb2 = new StringBuffer(sc.next());
+		sc.close();
+		int r1 = Integer.parseInt(sb1.reverse().toString());
+		int r2 = Integer.parseInt(sb2.reverse().toString());
+		System.out.println(r1 > r2 ? r1 : r2);
 	}
 }
