43517785be2f077d867e7385067754b6f3cd8d75
gyeomii <kimsg7388@gmail.com>
2022-06-18 15:30:45 +0900

    입출력 문제

---
+++ gyeomiiCodingTest/src/BaekJoon/basic/baekjoon10926.java
@@ -0,0 +1,10 @@
+package BaekJoon.basic;
+
+import java.util.Scanner;
+
+public class baekjoon10926 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(sc.nextLine() + "??!");
+	}
+}
