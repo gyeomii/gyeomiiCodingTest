7eb621f7e5c75b36f0a96ac15870fddd54ee9bfc
gyeomii <kimsg7388@gmail.com>
2022-06-21 11:06:35 +0900

    basic

---
+++ gyeomiiCodingTest/src/BaekJoon/basic/baekjoon11382.java
@@ -0,0 +1,10 @@
+package BaekJoon.basic;
+
+import java.util.Scanner;
+
+public class baekjoon11382 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(sc.nextLong() + sc.nextLong() + sc.nextLong());
+	}
+}
